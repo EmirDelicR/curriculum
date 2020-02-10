175f38d9a818ba5b54fcbce88c9e8dc28360d8a7
ed <ed@x-net.at>
2020-02-10 14:37:37 +0100

    Improving performance for lighthouse

--- src/components/Nav/menu-button.vue
+++ src/components/Nav/menu-button.vue
@@ -1,5 +1,5 @@
 <template>
   <button class="menu-button">
-    <span></span>
+    <span>Menu</span>
   </button>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const manifestJSON = require('./public/manifest.json');
+
 module.exports = {
-  // publicPath: process.env.NODE_ENV === 'production' ? '/project/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/curriculum/' : '/',
+  pwa: {
+    themeColor: manifestJSON.theme_color,
+    name: manifestJSON.short_name,
+    msTileColor: manifestJSON.background_color,
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'service-worker.js'
+    }
+  },
   pluginOptions: {
     i18n: {
       locale: 'en',
