ce4c4737e03910b4f5898c2bd4e67041f36da197
ed <ed@x-net.at>
2020-02-12 15:29:08 +0100

    Fixing performance issue

--- src/views/Certificates.vue
+++ src/views/Certificates.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="certificate-wrapper">
-    <SlideItem
-      v-for="slide in getSlideItems"
-      :item="slide"
-      :key="slide.title"
-    />
+    <SlideItem v-for="slide in getSlideItems" :item="slide" :key="slide.title" />
   </div>
 </template>
 
 <script>
-import SlideItem from '@/components/Certificates/slide-item';
-import { CERTIFICATIONS } from '@/utils/constants';
+import SlideItem from "@/components/Certificates/slide-item";
+import { CERTIFICATIONS } from "@/utils/constants";
 
 export default {
-  name: 'Contact',
+  name: "Contact",
   components: {
     SlideItem
   },
