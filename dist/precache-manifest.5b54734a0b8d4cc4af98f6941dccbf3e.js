fc2cc65cd6521b4838e6a34dd239ddc6b609e444
ed <ed@x-net.at>
2020-02-13 10:19:37 +0100

    Initial dist subtree commit

---
+++ dist/precache-manifest.5b54734a0b8d4cc4af98f6941dccbf3e.js
@@ -0,0 +1,157 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "/curriculum/2942bfabb3d05332b66eb128e0842cff.pdf"
+  },
+  {
+    "revision": "d95ad21290d6142984a2",
+    "url": "/curriculum/css/app.f27c5934.css"
+  },
+  {
+    "revision": "d266b97c3151075a2b11",
+    "url": "/curriculum/css/chunk-0b4b48a8.12c360ec.css"
+  },
+  {
+    "revision": "449fefd95d54dcc73d91",
+    "url": "/curriculum/css/chunk-3f490bee.1fbfa74c.css"
+  },
+  {
+    "revision": "03b8f335ca4bcebb52a301214f52790e",
+    "url": "/curriculum/fonts/Courgette-Regular.03b8f335.eot"
+  },
+  {
+    "revision": "1ceccbb7b3f98e3952c26a9c31fe04e1",
+    "url": "/curriculum/fonts/Courgette-Regular.1ceccbb7.woff2"
+  },
+  {
+    "revision": "59c3685a73f0f1b7c302dd2d6dabd628",
+    "url": "/curriculum/fonts/Courgette-Regular.59c3685a.ttf"
+  },
+  {
+    "revision": "a2a440b742b9e2b5643feeb6f2d39040",
+    "url": "/curriculum/fonts/Courgette-Regular.a2a440b7.woff"
+  },
+  {
+    "revision": "609af421413c6ae0ff490ae026872cee",
+    "url": "/curriculum/img/FCC.609af421.jpg"
+  },
+  {
+    "revision": "4fc9ce736fe7632db0d2def29923d07b",
+    "url": "/curriculum/img/JS.4fc9ce73.jpg"
+  },
+  {
+    "revision": "ff70349823e3dc1b875b6054fc37bf0e",
+    "url": "/curriculum/img/Node.ff703498.jpg"
+  },
+  {
+    "revision": "7a4273dd646a1d2cefe0e571d7379d32",
+    "url": "/curriculum/img/PWA.7a4273dd.jpg"
+  },
+  {
+    "revision": "ae2f2e4efb7b08195e4fba15b9a385f4",
+    "url": "/curriculum/img/Python.ae2f2e4e.jpg"
+  },
+  {
+    "revision": "267f60c63bd2acbebf89649810b0c5e2",
+    "url": "/curriculum/img/Python2.267f60c6.jpg"
+  },
+  {
+    "revision": "86a368731d25603d92715162479a6d1e",
+    "url": "/curriculum/img/React.86a36873.jpg"
+  },
+  {
+    "revision": "5e4963fa81a448624b4b52dee2ffc7f0",
+    "url": "/curriculum/img/SL1.5e4963fa.jpg"
+  },
+  {
+    "revision": "e700c13d7177bc849ca2b463760e40e3",
+    "url": "/curriculum/img/SL2.e700c13d.jpg"
+  },
+  {
+    "revision": "443068cbc85c2120230a9154c66165b4",
+    "url": "/curriculum/img/SL3.443068cb.jpg"
+  },
+  {
+    "revision": "b510279ace824956e979118e0d0b6992",
+    "url": "/curriculum/img/SL4.b510279a.jpg"
+  },
+  {
+    "revision": "dd43fdda913ec1d388c7b8886793ca16",
+    "url": "/curriculum/img/SL5.dd43fdda.jpg"
+  },
+  {
+    "revision": "d1e0373a4629ca7e9203e6af2cf28958",
+    "url": "/curriculum/img/SL6.d1e0373a.jpg"
+  },
+  {
+    "revision": "fc82d102b6c19a623831d95511484c25",
+    "url": "/curriculum/img/SL7.fc82d102.jpg"
+  },
+  {
+    "revision": "3bbd6f5124c42074be7f399553623e35",
+    "url": "/curriculum/img/TS.3bbd6f51.jpg"
+  },
+  {
+    "revision": "efe257b7e120a94132886d4f7d2c331c",
+    "url": "/curriculum/img/Vue.efe257b7.jpg"
+  },
+  {
+    "revision": "bd20a586142454f13f4469ca9307c878",
+    "url": "/curriculum/img/Vue2.bd20a586.jpg"
+  },
+  {
+    "revision": "231c537a401a9d5d26b3cf588371abf2",
+    "url": "/curriculum/img/map.231c537a.jpg"
+  },
+  {
+    "revision": "ff428b99c1da0d82e257548232be0314",
+    "url": "/curriculum/img/sl1.ff428b99.jpg"
+  },
+  {
+    "revision": "4d54e943529580db54dcd06cdf4d5054",
+    "url": "/curriculum/index.html"
+  },
+  {
+    "revision": "d95ad21290d6142984a2",
+    "url": "/curriculum/js/app.5c18feef.js"
+  },
+  {
+    "revision": "d266b97c3151075a2b11",
+    "url": "/curriculum/js/chunk-0b4b48a8.a86069fa.js"
+  },
+  {
+    "revision": "09783500126143d12238",
+    "url": "/curriculum/js/chunk-27595fa0.3e6add84.js"
+  },
+  {
+    "revision": "09e307a6e1570187dc66",
+    "url": "/curriculum/js/chunk-2d0c08c0.6605bfb4.js"
+  },
+  {
+    "revision": "25bfa9b5303c9966fd75",
+    "url": "/curriculum/js/chunk-2d210c47.640db6f0.js"
+  },
+  {
+    "revision": "9b8a1220b21a24b0e7ff",
+    "url": "/curriculum/js/chunk-2d21841c.ed92cccb.js"
+  },
+  {
+    "revision": "ba1dbd0df41e5f37067f",
+    "url": "/curriculum/js/chunk-2d21a3d2.6cbb5c94.js"
+  },
+  {
+    "revision": "449fefd95d54dcc73d91",
+    "url": "/curriculum/js/chunk-3f490bee.1befcd72.js"
+  },
+  {
+    "revision": "f86fc7a47d34cbbcdc96",
+    "url": "/curriculum/js/chunk-vendors.d4c584e5.js"
+  },
+  {
+    "revision": "f5968679d52407b053ee3730563ffd34",
+    "url": "/curriculum/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/curriculum/robots.txt"
+  }
+]);
